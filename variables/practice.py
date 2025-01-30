e2760b099b131a7d83b816fe092d8826a04f3877
LilaShearer <lila.shearer@ucas-edu.net>
2025-01-30 11:38:36 -0700

    finished code

--- variables/practice.py
+++ variables/practice.py
@@ -9,4 +9,4 @@ year = "2025"
 eye_color = "hazil"
 age = "14"
 favorite_subject = "Seminary"
-print("Hi!, my name is", name, "and I chose the number", number_1_to_10, "for my number between one and ten. For my number between 100 and 1000, I chose", number_100_to_1000, "and I had", breakfast_food, "for breakfast. My favorite color is", favorite_color, "and", eye_color, "is the color of my eyes. I go to the", school_name, "and the year is", year, ". I am", age, "years old, and my favorite subject in school is", favorite_subject, "." )
+print("Hi, my name is", name, "and I chose the number", number_1_to_10, "for my number between 1 and 10. For my number between 100 and 1000, I chose", number_100_to_1000, "and I had", breakfast_food, "for breakfast. My favorite color is", favorite_color, "and", eye_color, "is the color of my eyes. I go to the", school_name, "and the year is", year, ". I am", age, "years old, and my favorite subject in school is", favorite_subject, "." )
