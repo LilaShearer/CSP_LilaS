3c4d648aa0f71c3ceb1b0e4d2a05e052a7942baf
LilaShearer <lila.shearer@ucas-edu.net>
2025-01-30 11:49:50 -0700

    c practice

--- variables/notes.c
+++ variables/notes.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int age = 15;
+
+char name[] = "Vienna";
+
+float pi = 3.14;
+
 int main(void){
-    printf("Hello World");
+    printf("Hello, I am %s. I am %d years old. I like the number %f.\n", name, age, pi);
+    printf("%d\n", age);
+    printf("%f\n", pi);
     return 0;
 }
