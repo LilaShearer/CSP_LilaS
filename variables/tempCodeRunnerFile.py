5541ef7f3d48edc946b7d923de34f42fae68b48a
LilaShearer <lila.shearer@ucas-edu.net>
2025-01-30 11:33:31 -0700

    did python variable assignment

--- variables/tempCodeRunnerFile.py
+++ variables/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-# Lila Shearer Variables Practice - Python
-name = "Lila Shearer"
-number_1_to_10 = "4"
-number_100_to_1000 = "125"
-breakfast_food = "toast with almond butter"
-favorite_color = "dark green"
-school_name = "Utah County Academy of Sciences"
-year = "2025"
-eye_color = "hazil"
-age = "14"
-favorite_subject = "Seminary"
 print("Hi!, my name is", name, "and I chose the number", number_1_to_10, "for my number between one and ten. For my number between 100 and 1000, I chose", number_100_to_1000, "and I had", breakfast_food, "for breakfast. My favorite color is", favorite_color, "and", eye_color, "is the color of my eyes. I go to the", school_name, "and the year is", year, ". I am", age, "years old, and my favorite subject in school is", favorite_subject, "." )
