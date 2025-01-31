aec42ac331aac666ee8da7acf73d33f758ab3c51
LilaShearer <lila.shearer@ucas-edu.net>
2025-01-31 11:38:59 -0700

    fixed line issue

---
+++ variables/practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char name[] = "Lila Shearer";
+int number_between_1_and_10 = 3;
+int number_between_100_and_1000 = 443;
+char breakfast_food[] = "almond butter toast";
+char color[] = "dark green";
+char school[] = "UCAS";
+int year = 2025;
+char eye_color[] = "hazil";
+int age = 14;
+char subject[] = "Seminary";
+int main(void){
+    printf("Hello, my name is %s.\n The number I chose between 1 and 10 is %d and the number I chose between 100 and 100 is %d.\n I had %s for breakfast and my favorite color is %s.\n I go to %s in the year %d and my eyes are %s.\n I am %d years old and my favorite subject in school is %s.\n", name, number_between_1_and_10, number_between_100_and_1000, breakfast_food, color, school, year, eye_color, age, subject);
+    return 0;
+}
