e4f4160bf4438f05fcee0e693c2e1061c5a24cfd
LilaShearer <lila.shearer@ucas-edu.net>
2025-01-31 11:08:50 -0700

    completed first program

---
+++ variables/first_program.py
@@ -0,0 +1,9 @@
+# Lila Shearer, First Program Python
+name = input("What is your name: ")
+print("Nice to meet you,", name, "and welcome to my program.")
+book = input("What is your favorite book: ")
+print(name, "you clearly have good taste if you like", book,)
+print("My respect for you has greatly increased!")
+opinion = input("Do you like the book The Rithmatist by Brandon Sanderson: ")
+print("Well, I'm sure you have a good reason for your opinion.")
+print("Have an amazing day!")
