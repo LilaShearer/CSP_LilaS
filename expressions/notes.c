4aa65e6a800477486cfbbca387ee998f3bd3dc43
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-03 11:49:30 -0700

    finish part of notes

---
+++ expressions/notes.c
@@ -0,0 +1,10 @@
+//Lila Shearer, Expressions Notes C
+#include <stdio.h>
+#include <math.h>
+float x = (2,2);
+float equation = 5*7+11;
+// + - * / %   
+int main(void){
+    printf("%f", equation);
+    return 0;
+}
---
+++ expressions/notes.py
@@ -0,0 +1,39 @@
+# Lila Shearer, Expressions Notes Python
+
+# 1) set any variables, 
+# 2) print user instructions 
+# 3) code to complete task 
+# 4) print outcome
+
+
+#print(70+8)
+#print(70-8)
+#print(70*8)
+#print(70/8)
+#print(70**8)
+#print(70//8)
+#print(70%8) #modulo/modulus/MOD returns the remainder of your division
+
+num = 4
+
+print(num)
+
+num += 1
+
+print(num)
+
+num -= 1
+
+print(num)
+
+num *= 2
+
+print(num)
+
+num /= 2
+
+print(num)
+
+num //= 3
+
+print(num)
