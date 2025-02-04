2076132f1eba3d6c4ab997c74d9e96191cf32df9
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-04 11:46:15 -0700

    worked on practice c

---
+++ expressions/pratice.c
@@ -0,0 +1,18 @@
+//Lila Shearer, Expressions Practice C
+
+#include <stdio.h>
+#include <math.h>
+int one = 7-24/8*4+6;
+int two = 18/3-7+2*5;
+int three = 6*4/12+72/8-9;
+int four = (17-6/2)+4*3;
+int five = -2*(1*4-2/2)+(6+2-3);
+int six = -1*((3-4*7)/5)-2*24/6;
+int powerone = pow(5,2)
+int seven = (3*pow(5,2)/15)-(5-pow(2,2));
+int eight =
+int nine =
+int main(void){
+    printf("%d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n", one, two, three, four, five, six, seven, eight, nine);
+    return 0;
+}
