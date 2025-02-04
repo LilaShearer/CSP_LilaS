a7837ae9772d6e0bdd45809a820825bad4edf2f1
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-04 11:36:29 -0700

    did python expression practice

--- expressions/practice.py
+++ expressions/practice.py
@@ -1,3 +1,19 @@
 # Lila Shearer, Expression Practice Python
-
-print(5*2-7**3)
+first_equation = 7-24/8*4+6
+print(first_equation)
+second_equation = 18/3-7+2*5
+print(second_equation)
+third_equation = 6*4/12+72/8-9
+print(third_equation)
+fourth_equation = (17-6/2)+4*3
+print(fourth_equation)
+fifth_equation = -2*(1*4-2/2)+(6+2-3)
+print(fifth_equation)
+sixth_equation = -1*((3-4*7)/5)-2*24/6
+print(sixth_equation)
+seventh_equation = (3*5**2/15)-(5-2**2)
+print(seventh_equation)
+eighth_equation = (1**4*2**2+3**3)-2**5/4
+print(eighth_equation)
+ninth_equation = (22/2-2*5)**2+(4-6/6)**2
+print(ninth_equation)
