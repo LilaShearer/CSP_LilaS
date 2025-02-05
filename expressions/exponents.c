5285532aaf981de1f7cc255f12122ac3f50b5aea
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-05 11:26:26 -0700

    did c expression practice

---
+++ expressions/exponents.c
@@ -0,0 +1,12 @@
+//Lila Shearer, C Exponent
+#include <stdio.h>
+#include <math.h>
+float expression = 5*pow(7, 2)/4;
+//float expression = pow(7, 2);
+
+
+int main(void){
+    printf("%f", expression);
+    
+    return 0;
+}
--- expressions/practice.c
+++ expressions/practice.c
@@ -8,37 +8,10 @@ int three = 6*4/12+72/8-9;
 int four = (17-6/2)+4*3;
 int five = -2*(1*4-2/2)+(6+2-3);
 int six = -1*((3-4*7)/5)-2*24/6;
-
+int seven = (3*pow(5, 2)/15)-(5-pow(2, 2));
+int eight = ((pow(1, 4)*pow(2, 2)+pow(3,3))-pow(2, 5)/4);
+int nine = (pow(22/2-2*5, 2)+pow(4-6/6, 2));
 int main(void){
-    printf("%d\n %d\n %d\n %d\n %d\n %d\n", one, two, three, four, five, six);
-     double x = 5.0, y = 5.0;
+    printf("%d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n %d\n", one, two, three, four, five, six, seven, eight, nine);
 
-    // Storing the answer in sevenone.
-    float sevenone = pow(5, 5);
-  
-    // Storing the answer in seventwo.
-    float seventwo = pow(2, 2);
-    printf("(3*%f/15)-(5-2%f)", sevenone, seventwo);
-
-    // Storing the answer in eightone.
-    float eightone = pow(1, 4);
-    
-    // Storing the answer in eightwo.
-    float eighttwo = pow(2, 2);
-
-    // Storing the answer in eightthree.
-    float eightthree = pow(3, 3);
-   
-    // Storing the answer in eightfour.
-    float eightfour = pow(2, 5);
-    printf("(%f*%f+%f)-%f/4", eightone, eighttwo, eightthree,eightfour);
-
-
-    // Storing the answer in nineone.
-    float nineone = pow(22/2-2*5, 2);
-
-    // Storing the answer in ninetwo.
-    float ninetwo = pow(4-6/6, 2);
-    printf("%f+%f", nineone, ninetwo);
-    return 0;
 }
