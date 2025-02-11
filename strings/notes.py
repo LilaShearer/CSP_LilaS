7e6e367867ee9ac84a9e07b0f58218c50fa6a406
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-11 11:15:30 -0700

    worked on notes

--- strings/notes.py
+++ strings/notes.py
@@ -14,9 +14,22 @@ name = input("What is your first name?\n").strip().capitalize()
 
 print(f"Hello {name}, welcome to my program!")
 
-print("this is your name"+ name)
+print("this is your name: "+ name)
 #when we get syntax errors, it is because we didn't do things right. 
 
 #check the line before when debugging to make sure that the issue isn't on the line before. 
 
 # a run time error happens when the code can run until it stops doing the thing. 
+
+#concaitation means putting two strings together. In python we yse the addition symbol. 
+# This is concatenation print("this is your name"+ name)
+
+sentence = "The quick brown fox jumps over the lazy dog."
+
+print(len(sentence))
+
+print(sentence[4:7])
+
+print(sentence.find("fox"))
+
+print(sentence[16:19])
