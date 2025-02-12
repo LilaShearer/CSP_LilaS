7eb8562661b793338a5db580b2dd9a4efd2a19a8
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-12 11:48:52 -0700

    worked on practice

---
+++ strings/name_decorator.c
@@ -0,0 +1,18 @@
+//Lila Shearer, Name Decorator C
+#include <stdio.h>
+
+char name[50];
+char option[50] = "$$$";
+char option_part_two[50] = "$$$";
+
+
+int main(void){
+   printf("Welcome to name decorator!\nThis program is deisgned to take your name and add fun decorations to it.\nWhat is your first name?\n");
+   scanf("%s", name);
+   strcat(option,name);
+   strcat(option,option_part_two);
+   printf(option);
+   
+    return 0;
+}
+
---
+++ strings/name_decorator.py
@@ -0,0 +1,1 @@
+# Lila Shearer, Name Decorator Python
