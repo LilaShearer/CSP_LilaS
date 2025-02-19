3893e5bab46a0ad1d6c736e0d8395e18d38d544a
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-19 11:13:07 -0700

    hello world c

--- functions/updated_hello_world.c
+++ functions/updated_hello_world.c
@@ -1,3 +1,14 @@
 //Lila Shearer, Updated Hello World C
 #include <stdio.h>
 
+
+void due(char assignment[50], char day[20]){
+    printf("The %s assignment is due %s\n", assignment, day);
+
+}
+
+int main(void){
+    due("Functions notes", "Today");
+    due("Hello Word Update", "Tomorrow" );
+    due("Essay", "Wednesday");
+}
