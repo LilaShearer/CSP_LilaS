9b51c4cd6b46cfac6c0ea506498f57f6ad47cac8
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-19 19:15:35 +0000

    worked on python hello world

--- functions/updated_hello_world.py
+++ functions/updated_hello_world.py
@@ -5,7 +5,11 @@ def name():
     return (f"Hello, {name_input}!")
     
 print(name())
+print("Zachary"())
 print(name())
 print(name())
 print(name())
-print(name())
+
+#I decided to do user inputs on this assignment because I could. The way that it would look if it wasn't a user input would be like this:
+#def name(nameVariable):
+#   return (f"Hello, {nameVariable})
