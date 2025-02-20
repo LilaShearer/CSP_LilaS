c9175218220da409b55f73945e069e0b919e1e31
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-20 00:52:44 +0000

    finished hello world assignment

--- functions/updated_hello_world.c
+++ functions/updated_hello_world.c
@@ -9,7 +9,7 @@ void greeting(char answer[50]){
 }
 
 int main(void){
-    printf("Welcome to Hello World. This program is designed to say hello to names. You will get the opportunity to insert a name and see other names that are greeted.\n");
+    printf("Welcome to updated Hello World. This program is designed to say hello to names. You will get the opportunity to insert a name and see other names that are greeted.\n");
     char answer[50];
     printf("Please type a name:\n");
     scanf("%s", answer);
--- functions/updated_hello_world.py
+++ functions/updated_hello_world.py
@@ -1,15 +1,21 @@
 # Lila Shearer, Updated Hello World
-
+print("Welcome to updated Hello World! This program takes in 5 names and says hellos to them individually.\n")
 def name():
     name_input = input("Please write a name:\n").strip().capitalize()
     return (f"Hello, {name_input}!")
     
 print(name())
-print("Zachary"())
+print(name())
 print(name())
 print(name())
 print(name())
 
-#I decided to do user inputs on this assignment because I could. The way that it would look if it wasn't a user input would be like this:
+#I did user inputs for this, but in case that doesn't fit the assignment requirements, here is what it looks like if it's not user inputs:
 #def name(nameVariable):
-#   return (f"Hello, {nameVariable})
+#    return (f"Hello, {nameVariable}!")
+#
+#print(name("Peter"))
+#print(name("John"))
+#print(name("Jacob"))
+#print(name("Emily"))
+#print(name("Charlie"))
