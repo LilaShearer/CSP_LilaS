c7d58598d00f2ed9e7eaba7ac4c362ef9a75a664
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-25 11:49:05 -0700

    worked on practice

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -4,7 +4,15 @@ int age;
 int main(void){
     printf("How old are you in years?\n");
     scanf("%d", age);
-    if 
+    if(age < 5){
+        printf("You are not old enough to go to school.\n");
+    }else if(age >=5){
+        if(age >=18){
+            printf("You are old enough to vote, drive, get a learner's permit and go to school!");
+        } else if(age>=)
+    } else {
+        printf("You aren't old enough to do anything!\n");
+    }
 
 
 
