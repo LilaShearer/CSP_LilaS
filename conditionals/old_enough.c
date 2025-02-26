c848427b7059c879fd559a56b1b3e0a7a34cee2b
LilaShearer <lila.shearer@ucas-edu.net>
2025-02-26 18:05:59 +0000

    worked on old enough

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -3,18 +3,18 @@
 int age;
 int main(void){
     printf("How old are you in years?\n");
-    scanf("%d", age);
+    scanf("%d", &age);
     if(age < 5){
-        printf("You are not old enough to go to school.\n");
-    }else if(age >=5){
-        if(age >=18){
-            printf("You are old enough to vote, drive, get a learner's permit and go to school!");
-        } else if(age>=)
-    } else {
-        printf("You aren't old enough to do anything!\n");
+        printf("You are not old enough to go to school, get your learner's permit, drive, or vote.\nWhy are you running this program anyway?\nShouldn't you be playing outside or something?\n");
+    } else if(age >= 18){
+        printf("You are old enough to vote, drive, get your learner's permit, and go to school.\n");
+    } else if(age >=16){
+        printf("You are old enough to drive. Hopefully you know what you're doing!\n");
+    } else if(age>=15){
+        printf("You are old enough to get your learner's permit. Exciting!\n");
+    } else{
+        printf("You are old enough to go to school.\n");
     }
-
-
 
 
     return 0;
