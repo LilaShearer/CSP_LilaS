b1dc58748a761febb0d7ea3c58e310768037c9d9
LilaShearer <lila.shearer@ucas-edu.net>
2025-03-06 11:26:38 -0700

    fix

--- loops/family_loop.c
+++ loops/family_loop.c
@@ -5,9 +5,8 @@ int main(void){
     char family_members[][20] = {"Wade", "Ivana", "Paisley", "Max", "Afton", "Jack"};
     int ilength = sizeof(family_members)/sizeof(family_members[0]);
     int i;
-    printf("Here are the names of my family members:\n");
     for(i=0;i<ilength;i++){
-        printf("%s\n", family_members[i]);
+        printf("This is one of my family members: %s\n", family_members[i]);
     }
     return 0;
 }
