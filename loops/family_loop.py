804a676280d54358176909bd79d31f12cfd465cb
LilaShearer <lila.shearer@ucas-edu.net>
2025-03-06 11:27:31 -0700

    fix

--- loops/family_loop.py
+++ loops/family_loop.py
@@ -1,5 +1,5 @@
 # Lila Shearer, Family Loop Python
 family_members = ["Wade", "Ivana", "Paisley", "Max", "Afton", "Jack"]
-print("Here are the names of my family members:")
+
 for family_member in family_members:
-    print(family_member)
+    print(f"This is one of my family members: {family_member}\n")
