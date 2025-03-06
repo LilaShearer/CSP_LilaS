c6b4cfbd938afd8a636c05d86ba498d458f90a1e
LilaShearer <lila.shearer@ucas-edu.net>
2025-03-06 11:07:22 -0700

    did python practice

---
+++ loops/family.loop.c
@@ -0,0 +1,7 @@
+//Lila Shearer, Family Loop
+#include <stdio.h>
+
+int main(void){
+    
+    return 0;
+}
---
+++ loops/family_loop.py
@@ -0,0 +1,5 @@
+# Lila Shearer, Family Loop Python
+family_members = ["Wade", "Ivana", "Paisley", "Max", "Afton", "Jack"]
+
+for family_member in family_members:
+    print(family_member)
