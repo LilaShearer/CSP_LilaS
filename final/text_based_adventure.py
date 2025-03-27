a3eb91bc3cc7387f08e299353beaaf5908379d29
LilaShearer <lila.shearer@ucas-edu.net>
2025-03-27 11:44:00 -0600

    finished text-based adventure

--- final/text_based_adventure.py
+++ final/text_based_adventure.py
@@ -195,7 +195,7 @@ else:
 # This part of my code decides how the critic will respond by looking at what decisions werre made
 if userNotTrying == 1:
     customerAnswer = "HATED"
-    explanationMessage = "\nYou tried to use ingredients we didn't have, resulting in:\nyour cake failing,\nyour partner never meeting or marrying you,\nyour entire future being ruined,\nand a curse placed on all of your family members.\nNext time, use the ingredients provided!"
+    explanationMessage = "\nYou tried to use ingredients we didn't have, resulting in:\nyour cake failing,\nyou eternally living in your mother's basement,\nyour partner never meeting or marrying you,\nyour entire future being ruined,\nand a curse placed on all of your family members.\nNext time, use the ingredients provided!\n♡"
 elif frostPiped == "very nice looking" and cakeStat == 2 and toppings != " no toppings":
     customerAnswer = "LOVED"
     explanationMessage = "\nYour cake was perfect!\nYou won the game!\n"
