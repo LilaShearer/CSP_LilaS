8feed1726c2c7eeba3ddef51c6dfb8199bf234f8
LilaShearer <lila.shearer@ucas-edu.net>
2025-03-27 11:37:06 -0600

    save
    Co-authored-by: Lizzie42-SandersonFan <Lizzie42-SandersonFan@users.noreply.github.com>
    Co-authored-by: Kal360 <Kal360@users.noreply.github.com>
    Co-authored-by: RiverWinds256 <RiverWinds256@users.noreply.github.com>

--- final/example.py
+++ final/example.py
@@ -1,48 +1,5 @@
-
-#Chiara variables
-cakeFlavor=1
-frostingFlavor=2
-
-#River varables
-cakepan=1
-cakeStat= 2
-ovenTemp = 0
-
-#Lizzie variables
-frostPiped=1
-allToppings = ["sprinkles", "strawberries", "fudge sauce"]
-isSprinkles = False
-isStrawberries = False
-isFudge = False
-notoppings = 0
-toppings = 0
-response = 0
-explanationMessage = 0
-customerAnswer = 0
-
-
-def toppingRequest(theTopping):
-    global response
-    response = input(f"Would you like {theTopping} (Yes or No)?\n").strip().upper()
-    print(response)
-
-for item in allToppings:
-    toppingRequest(item)
-    if response == "YES" and item == "sprinkles":
-        isSprinkles = True
-    else:
-        isSprinkles = False
-
-    if response == "YES" and item == "strawberries":
-        isStrawberries = True
-    else:
-        isStrawberries = False
-
-    if response == "YES" and response == "fudge sauce":
-        isFudge = True
-    else:
-        isFudge = False
-
-print(isSprinkles)
-print(isStrawberries)
-print(isFudge)
+print("\nThank you for playing!\n")
+print(" @@@@@@@ ")
+print("_|_____|_")
+print("@~~~~~~~~@")
+print("|________|")
